13318f69a6e4a3a8fb47095649e043f798a623e7
Aurelien Callens <aureliencallens@aureliens-mbp.home>
2017-06-25 11:00:25 +0200

    Add filter functionnality

--- app/api/TodoApi.jsx
+++ app/api/TodoApi.jsx
@@ -1,24 +1,48 @@
 const $ = require("jQuery");
 
-const setTodos = function(todos) {
-    if($.isArray(todos)){
+const setTodos = function (todos) {
+    if ($.isArray(todos)) {
         localStorage.setItem("todos", JSON.stringify(todos));
         return todos;
     }
 };
 
-const getTodos = function() {
+const getTodos = function () {
     const stringTodos = localStorage.getItem("todos");
     let todos = [];
     try {
         todos = JSON.parse(stringTodos);
-    } catch(e) {
+    } catch (e) {
 
     }
-    return $.isArray(todos) ? todos: [];
+    return $.isArray(todos) ? todos : [];
 };
+
+const filterTodos = (todos, showCompleted, searchText) => {
+    let filteredTodos = todos.filter(todo => {
+        return !todo.completed || showCompleted;
+    });
+
+    filteredTodos = filteredTodos.filter(todo => {
+        const text = todo.text.toLowerCase();
+        return searchText.length === 0 || text.indexOf(searchText.toLowerCase()) > -1;
+    });
+
+    filteredTodos.sort((a, b) => {
+        if(!a.completed && b.completed){
+            return -1;
+        }
+        if(a.completed && !b.completed){
+            return 1;
+        }
+        return 0;
+    });
+
+    return filteredTodos;
+}
 
 module.exports = {
     setTodos,
-    getTodos
+    getTodos,
+    filterTodos
 };
--- app/tests/components/api/TodoApi.test.jsx
+++ app/tests/components/api/TodoApi.test.jsx
@@ -14,7 +14,7 @@ describe("TodoApi", () => {
         it("should set valid todos array", () => {
             const todos = [{
                 id: 23,
-                test: "test all files",
+                text: "test all files",
                 completed: false
             }];
             TodoApi.setTodos(todos);
@@ -38,7 +38,7 @@ describe("TodoApi", () => {
         it("should return todos if valid array in local storage", () => {
             const todos = [{
                 id: 23,
-                test: "test all files",
+                text: "test all files",
                 completed: false
             }];
             localStorage.setItem("todos", JSON.stringify(todos));
@@ -46,5 +46,46 @@ describe("TodoApi", () => {
             const actualTodos = TodoApi.getTodos();
             expect(actualTodos).toEqual(todos);
         })
+    });
+
+    describe("filterTodos", () => {
+        const todos = [{
+            id: 1,
+            text: "Some text here",
+            completed: true
+        },{
+            id: 2,
+            text: "Other text here",
+            completed: false
+        },{
+            id: 3,
+            text: "Some text here",
+            completed: true
+        }];
+
+        it("should return all items if showCompleted is true", () => {
+            const filteredTodos = TodoApi.filterTodos(todos, true, "");
+            expect(filteredTodos.length).toBe(3);
+        });
+
+        it("should return only not completed items if showCompleted is false", () => {
+            const filteredTodos = TodoApi.filterTodos(todos, false, "");
+            expect(filteredTodos.length).toBe(1);
+        });
+
+        it("should sort by completed status", () => {
+            const filteredTodos = TodoApi.filterTodos(todos, true, "");
+            expect(filteredTodos[0].completed).toBe(false);
+        });
+
+        it("should filter todos by searchText", () => {
+            const filteredTodos = TodoApi.filterTodos(todos, true, "some");
+            expect(filteredTodos.length).toBe(2);
+        });
+
+        it("should return all todos if searchText is empty", () => {
+            const filteredTodos = TodoApi.filterTodos(todos, true, "");
+            expect(filteredTodos.length).toBe(3);
+        });
     });
 });
